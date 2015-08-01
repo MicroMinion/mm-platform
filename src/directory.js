7b0c514d684745f68f5ebfe7571fd581607a679a
Thomas Delaet <thomas@delaet.org>
2015-08-01 23:00:53 +0200

    implement interface + implementation notes for nico. Happy to discuss

--- src/directory.js
+++ src/directory.js
@@ -1,9 +1,30 @@
 var _ = require("lodash");
+var rateLimit = require("rate-limit");
 
+
+/*
+ * Thoughts on implementation:
+ *
+ * - local kunnen we for now niet doen omdat mDNS niet werkt dus ik zou dat blank laten
+ * - options object bevat mogelijke volgende attributen:
+ *     - success, warning, error: callbacks
+ *     - realtime: true| false => of operatie onmiddellijk moet uitgevoerd worden of aan queue kan toegevoegd worden
+ *
+ * - rate-limit lijkt propere manier om queues te creeeren
+ * - code die deze functies aanroept vind je in stores (contacts.js, profile.js) look for get/put methods
+ * - aangezien key gaat gehashed worden gaan we ergens mapping moeten bijhouden van key => hash voor callbacks
+ * - de stores gaan er van uit dat een lookup meerdere matches kan geven. het is dus perfect okay om voor een get request meerdere keren success callback aan te roepen
+ */
 
 var instances = {
     local: require("./directory/mdns.js"),
     global: require("./directory/dht.js")
+};
+
+
+var queues = {
+    local: rateLimit.createQueue({interval: 100}),
+    global: rateLimit.createQueue({interval: 100})
 };
 
 instances.local.on("error", function(err) {
@@ -12,11 +33,14 @@ instances.local.on("error", function(err) {
     instances.local = null;
 });
 
+
+
+
 var put = function(key, value, options) {
     if(instances.local) {
         instances.local.put(key, value, options);
     } else {
-        if(options && options.error) {
+        if(options && options.warning) {
             options.error("Not possible to publish key %s on the local network", key);
         };
     };
@@ -29,7 +53,11 @@ var put = function(key, value, options) {
     };
 };
 
+var get = function(key, options) {
+};
+
 
 module.exports = {
-    put: put
+    put: put,
+    get: get
 };
