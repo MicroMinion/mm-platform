a6dfe15d524a0b376f319cf1a16704fcc63a5dec
Thomas Delaet <thomas@delaet.org>
2015-09-26 11:27:58 +0200

    Add support for torrenting protocol
    This only adds support for flunky-platform side of torrenting.
    Individual services that need to download files will have to implement a modified version of the peer-wire-protocol/peer-wire-swarm and torrent-stream module
    That work is still to be done

--- src/messaging/torrenting.js
+++ src/messaging/torrenting.js
@@ -2,6 +2,8 @@
 
 var curve = require('./crypto-curvecp.js')
 var expect = require('chai').expect
+var isBuffer = require('is-buffer')
+
 var PROTOCOL = 'to'
 
 var Torrenting = function (dispatcher) {
@@ -13,13 +15,18 @@ Torrenting.prototype._setupDispatcher = function () {
   this.dispatcher.on(PROTOCOL, function (scope, publicKey, message) {
     expect(publicKey).to.be.a('string')
     expect(curve.fromBase64(publicKey)).to.have.length(32)
-    var infoHash = '' // TODO: Extract infohash
-    message = message // TODO: Remove infohash so we get a clean wire protocol message
+    var infoHash = message.slice(0, 20)
+    message = message.slice(20)
     torrenting.emit(scope + '.' + infoHash, publicKey, message)
   })
 }
 Torrenting.prototype.send = function (infoHash, publicKey, message) {
-  // TODO: Implemeting wrapping infoHash in message
+  expect(publicKey).to.be.a('string')
+  expect(isBuffer(message)).to.be.true
+  expect(isBuffer(infoHash)).to.be.true
+  expect(message.length).to.be.greaterThan(0)
+  expect(infoHash.length).to.equal(20)
+  var buffer = Buffer.concat([infoHash, message])
   message = message
-  this.dispatcher.send(PROTOCOL, publicKey, message)
+  this.dispatcher.send(PROTOCOL, publicKey, buffer)
 }
