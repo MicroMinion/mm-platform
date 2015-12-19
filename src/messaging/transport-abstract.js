c3a8b3d0c5ccea1a33f32de812f17f04dd4bf9e0
Thomas Delaet <thomas@delaet.org>
2015-12-19 17:29:17 +0100

    Fix error in transport-abstract on how to extract curvecp packet stream

--- src/messaging/transport-abstract.js
+++ src/messaging/transport-abstract.js
@@ -239,6 +239,7 @@ AbstractTransport.prototype._deleteStream = function (stream) {
 
 AbstractTransport.prototype._getPeer = function (stream) {
   debug('getPeer')
+  var stream = stream.stream
   var publicKey = stream.is_server ? stream.clientPublicKey : stream.serverPublicKey
   if (!publicKey) {
     return
