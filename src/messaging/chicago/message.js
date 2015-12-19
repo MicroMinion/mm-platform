ce99ead4ef2536ca1665cb0f6d53255b0661e03c
Thomas Delaet <thomas@delaet.org>
2015-12-19 16:24:22 +0100

    Change EOL format of message.js and fix buffer handling errors

--- src/messaging/chicago/message.js
+++ src/messaging/chicago/message.js
@@ -31,17 +31,17 @@ Message.prototype.fromBuffer = function (buf) {
   this.id = buf.readUInt32BE()
   this.acknowledging_id = buf.readUInt32BE(4)
   this.acknowledging_range_1_size = new Uint64BE(buf, 8)
-  this.acknowledging_range_12_gap = buf.readUInt32BE(buf, 16)
-  this.acknowledging_range_2_size = buf.readUInt16BE(buf, 20)
-  this.acknowledging_range_23_gap = buf.readUInt16BE(buf, 22)
-  this.acknowledging_range_3_size = buf.readUInt16BE(buf, 24)
-  this.acknowledging_range_34_gap = buf.readUInt16BE(buf, 26)
-  this.acknowledging_range_4_size = buf.readUInt16BE(buf, 28)
-  this.acknowledging_range_45_gap = buf.readUInt16BE(buf, 30)
-  this.acknowledging_range_5_size = buf.readUInt16BE(buf, 32)
-  this.acknowledging_range_56_gap = buf.readUInt16BE(buf, 34)
-  this.acknowledging_range_6_size = buf.readUInt16BE(buf, 36)
-  this.flags = buf.readUInt16BE(buf, 38)
+  this.acknowledging_range_12_gap = buf.readUInt32BE(16)
+  this.acknowledging_range_2_size = buf.readUInt16BE(20)
+  this.acknowledging_range_23_gap = buf.readUInt16BE(22)
+  this.acknowledging_range_3_size = buf.readUInt16BE(24)
+  this.acknowledging_range_34_gap = buf.readUInt16BE(26)
+  this.acknowledging_range_4_size = buf.readUInt16BE(28)
+  this.acknowledging_range_45_gap = buf.readUInt16BE(30)
+  this.acknowledging_range_5_size = buf.readUInt16BE(32)
+  this.acknowledging_range_56_gap = buf.readUInt16BE(34)
+  this.acknowledging_range_6_size = buf.readUInt16BE(36)
+  this.flags = buf.readUInt16BE(38)
   this.offset = new Uint64BE(buf, 40)
   this.data_length = this.flags - (this.flags & STOP)
   this.data = buf.slice(buf.length - this.data_length)
@@ -49,7 +49,7 @@ Message.prototype.fromBuffer = function (buf) {
 
 Message.prototype.toBuffer = function () {
   var messageSize = HEADER_SIZE + MINIMAL_PADDING
-  if (this.data && this.data.length > 0) {
+  if (this.data !== undefined && this.data.length > 0) {
     messageSize += this.data.length + (16 - this.data.length % 16)
   }
   assert(messageSize <= MAX_MESSAGE_SIZE)
@@ -68,15 +68,19 @@ Message.prototype.toBuffer = function () {
   message.writeUInt16BE(this.acknowledging_range_5_size, 32)
   message.writeUInt16BE(this.acknowledging_range_56_gap, 34)
   message.writeUInt16BE(this.acknowledging_range_6_size, 36)
-  this.flags = this.data.length
+  if (this.data !== undefined) {
+    this.flags = this.data.length
+  } else {
+    this.flags = 0
+  }
   if (this.success) {
     this.flags += STOP_SUCCESS
   } else if (this.failure) {
     this.flags += STOP_FAILURE
   }
   message.writeUInt16BE(this.flags, 38)
-  this.offset.toBuffer().copy(message, 40)
   if (this.data && this.data.length > 0) {
+    this.offset.toBuffer().copy(message, 40)
     this.data.copy(message, messageSize - this.data.length)
   }
   return message
