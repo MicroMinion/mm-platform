a4482670eb0110422da27adba899095f8a88196f
Thomas Delaet <thomas@delaet.org>
2016-01-02 11:20:20 +0100

    Clean out src directory to only include connectivity logic

--- src/proxy/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Client: require('./client.js'),
-  Server: require('./server.js')
-}
--- src/services/directory/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  DirectoryClient: require('./server.js').DirectoryClient,
-  DirectoryServer: require('./server.js').DirectoryServer
-}
